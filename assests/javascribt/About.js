e32cf9ced02f2ae98d9cb2f994bddb339dcc4784
amir-kamel <amir.kghabrial@gmail.com>
2025-02-22 02:23:40 +0200

    add about us page with fetching footer inside it

---
+++ assests/javascribt/About.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    async function loadContent(url, elementId) {
+        try {
+          const response = await fetch(url);
+          const data = await response.text();
+          document.getElementById(elementId).innerHTML = data;
+        } catch (error) {
+          console.error("Error loading content:", error);
+        }
+      }
+
+    // Load navigation and footer, then initialize search functionality
+    (async function () {
+      await loadContent("Footer.html", "footer");
+      // Initialize search functionality after navigation content is loaded
+    })();
+  });
